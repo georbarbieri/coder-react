883bc7a620462fc820a1987518a7d8d4a9b06ffc
Georgina Barbieri <barbierigeorgina@gmail.com>
2023-03-16 00:57:51 -0300

    arreglo-itemD-itemDC

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -1,47 +1,9 @@
-import React, { useContext } from "react";
-import { CartContext } from "../../context/CartContext";
-import ItemCount from "../ItemCount/ItemCount";
-import Swal from "sweetalert2";
-import "./ItemDetail.css";
+import React from "react";
+
+import ItemDetailContainer from "../ItemDetailContainer/ItemDetailContainer";
 
 const ItemDetail = ({ product }) => {
-  const { addToCart, getQuantityById } = useContext(CartContext);
-
-  const agregarAlCarrito = (cantidad) => {
-    const obj = {
-      ...product,
-      quantity: cantidad,
-    };
-    addToCart(obj);
-
-    Swal.fire({
-      position: "center",
-      icon: "success",
-      title: "Tu producto fue agregado al carrito",
-      showConfirmButton: true,
-    });
-  };
-
-  const quantity = getQuantityById(product.id);
-
-  return (
-    <div className="container-item-detail">
-      <div className="item-img">
-        <img src={product.img} alt="" />
-      </div>
-      <div className="item-description" style={{ paddingBottom: "100px" }}>
-        <h1>Titulo: {product.title}</h1>
-        <h2>Precio: $ {product.price}</h2>
-        <h3>Descripción de producto: {product.description}</h3>
-
-        <ItemCount
-          stock={product.stock}
-          initial={quantity}
-          agregarAlCarrito={agregarAlCarrito}
-        />
-      </div>
-    </div>
-  );
+  return <ItemDetailContainer product={product} />;
 };
 
 export default ItemDetail;
--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,14 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../../firebaseConfig";
-import ItemDetail from "../ItemDetail/ItemDetail";
-
+import { CartContext } from "../../context/CartContext";
+import ItemCount from "../ItemCount/ItemCount";
+import Swal from "sweetalert2";
 import { getDoc, collection, doc } from "firebase/firestore";
-
+import "./ItemDetailContainer.css";
 const ItemDetailContainer = () => {
   const { id } = useParams();
   const [product, setProduct] = useState({});
+  const { addToCart, getQuantityById } = useContext(CartContext);
 
+  const agregarAlCarrito = (cantidad) => {
+    const obj = {
+      ...product,
+      quantity: cantidad,
+    };
+    addToCart(obj);
+
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Tu producto fue agregado al carrito",
+      showConfirmButton: true,
+    });
+  };
+
+  const quantity = getQuantityById(product.id);
   useEffect(() => {
     const itemCollection = collection(db, "products");
     const ref = doc(itemCollection, id);
@@ -23,7 +41,24 @@ const ItemDetailContainer = () => {
       .catch((err) => console.log(err));
   }, [id]);
 
-  return <ItemDetail product={product} />;
+  return (
+    <div className="container-item-detail">
+      <div className="item-img">
+        <img src={product.img} alt="" />
+      </div>
+      <div className="item-description">
+        <h1>Titulo: {product.title}</h1>
+        <h2>Precio: $ {product.price}</h2>
+        <h3>Descripción de producto: {product.description}</h3>
+        <h3>Categoria: {product.category}</h3>
+        <ItemCount
+          stock={product.stock}
+          initial={quantity}
+          agregarAlCarrito={agregarAlCarrito}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ItemDetailContainer;
